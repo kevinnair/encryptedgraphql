ccaa5fdb94211fdde975691e67298b4cc245c0ed
Kevin Nair <kmnair@bitpay.com>
2020-02-28 14:40:02 -0500

    adds some chained promises

--- app.js
+++ app.js
@@ -64,7 +64,10 @@ app.use('/graphql', graphQLHttp({
     events: () => {
       return Event.find().then((events) => {
         return events.map((event) => {
-          return { ...event._doc, _id: event._doc._id.toString() };
+          return {
+            ...event._doc,
+            _id: event._doc._id.toString()
+          };
         });
       }).catch((err) => {
         throw err;
@@ -79,13 +82,21 @@ app.use('/graphql', graphQLHttp({
       });
 
       return event.save().then((res) => {
-        return { ...res._doc, _id: res.id };
+        return {
+          ...res._doc,
+          _id: res.id
+        };
       }).catch((err) => {
         throw err;
       });
     },
     createUser: (args) => {
-      return bcrypt.hash(args.userInput.password, 12).then((hashedPassword) => {
+      return User.findOne({ email: args.userInput.email }).then((user) => {
+        if(user) {
+          throw new Error('User exists already.');
+        }
+        return bcrypt.hash(args.userInput.password, 12);
+      }).then((hashedPassword) => {
         const user = new User({
           email: args.userInput.email,
           password: hashedPassword
